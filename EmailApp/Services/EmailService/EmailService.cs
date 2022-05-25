124c546f6f3a26c7dbea442336d23eb5d08bc863
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2022-05-25 09:20:34 +0200

    Init

---
+++ EmailApp/Services/EmailService/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace EmailApp.Services.EmailService
+{
+    public class EmailService : IEmailService
+    {
+
+        private readonly IConfiguration _configuration;
+
+
+        public EmailService(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        public void SendEmail(EmailDto request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_configuration.GetSection("EmailUserName").Value));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+            using var smtp = new SmtpClient();
+            smtp.Connect(_configuration.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_configuration.GetSection("EmailUserName").Value, _configuration.GetSection("EmailPassword").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
---
+++ EmailApp/Services/EmailService/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace EmailApp.Services.EmailService
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailDto request);
+    }
+}
